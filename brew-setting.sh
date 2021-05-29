62efc111f7795f69c7038103aceacc11c84c6f8e
Satoshi Fujinaga <fujinaga.1984@aol.com>
2021-05-29 15:36:11 +0900

    Initial commit
    add brew setting

---
+++ brew-setting.sh
@@ -0,0 +1,114 @@
+#!/bin/sh
+xcode-select --install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# inastall mas-cli
+brew install mas
+
+# git
+brew install git
+
+# composer tool
+brew install ansible
+
+# image converter
+brew install webp
+brew install imagemagick
+
+# nodebrew
+brew install nodebrew
+
+# ruby
+brew install rbenv
+brew install ruby-build
+
+# self CA tool
+brew install mkcert
+
+# chrome drive
+brew install selenium-server-standalone
+brew install chromedriver
+
+# browser
+brew install --cask google-chrome
+brew install --cask firefox
+brew install --cask microsoft-edge
+brew install --cask chromium
+
+# virtual machine
+brew install --cask docker
+brew install --cask vagrant
+brew install --cask virtualbox
+brew install --cask utm
+
+# editor
+brew install --cask visual-studio-code
+brew install --cask sublime-text
+brew install --cask atom
+brew install --cask coda
+
+# cli tool
+brew install --cask iterm2
+
+# proxy tool
+brew install --cask owasp-zap
+
+# git client
+brew install --cask github
+brew install --cask fork
+brew install --cask sourcetree
+
+# db client
+brew install --cask sequel-pro
+brew install --cask sequel-ace
+
+# api client
+brew install --cask insomnia
+brew install --cask postman
+brew install --cask graphql-playground
+
+# programming tool
+brew install --cask r
+brew install --cask rstudio
+brew install --cask processing
+
+# file transfer tool
+brew install --cask transmit
+
+# draw tool
+brew install --cask drawio
+brew install --cask figma
+
+# web tool
+brew install --cask google-web-designer
+
+# markdown tool
+brew install --cask typora
+
+# wiki tool
+brew install --cask notion
+
+# note
+brew install --cask simplenote
+
+# chat tool
+brew install --cask slack
+
+# video chat tool
+brew install --cask zoom
+
+# disk cleaner tool
+brew install --cask daisydisk
+
+# cloud storage
+brew install --cask dropbox
+
+# password tool
+brew install --cask 1password
+brew install --cask lastpass
+
+# capture tool
+brew install --cask gyazo
+
+# tunnel tool
+brew install --cask ngrok
